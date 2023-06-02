1d53326ae815f070af4e2e4b0495d99a38c9083a
Decker Dominik <d.decker@gentics.com>
2023-06-02 11:54:39 +0200

    GPU-878: Fixed uuid dependency and usage

--- cms-ui/libs/form-generator/src/lib/common/utils/uuid.ts
+++ cms-ui/libs/form-generator/src/lib/common/utils/uuid.ts
@@ -1,21 +1,5 @@
-import * as uuidv4 from 'uuidv4';
+import { v4 } from 'uuid';
 
 export function newUUID(): string {
-    let fnOrRes = uuidv4;
-
-    // Sanity check
-    if (fnOrRes == null) {
-        return null;
-    }
-
-    // This may happen in the final build, due to wrong tree-shaking/module-resolution.
-    if (typeof fnOrRes === 'object') {
-        fnOrRes = (fnOrRes as any).default;
-    }
-    if (typeof fnOrRes === 'function') {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        fnOrRes = (fnOrRes as any)();
-    }
-
-    return typeof fnOrRes === 'string' ? fnOrRes : null;
+    return v4();
 }

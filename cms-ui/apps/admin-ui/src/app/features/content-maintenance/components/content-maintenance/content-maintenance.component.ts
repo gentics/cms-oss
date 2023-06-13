a0719502ca50c50eceec9f38de9b9f5e9bc7bc47
Decker Dominik <d.decker@gentics.com>
2023-06-13 17:53:53 +0200

    GPU-885: Maintenance refresh toggle persistant user setting

--- cms-ui/apps/admin-ui/src/app/features/content-maintenance/components/content-maintenance/content-maintenance.component.ts
+++ cms-ui/apps/admin-ui/src/app/features/content-maintenance/components/content-maintenance/content-maintenance.component.ts
@@ -1,7 +1,8 @@
 import { PUBLISH_PROCESS_REFRESH_INTERVAL } from '@admin-ui/common';
-import { SelectState } from '@admin-ui/state';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AppStateService, SelectState, SetUserSettingAction } from '@admin-ui/state';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, combineLatest } from 'rxjs';
+import { filter, mergeMap } from 'rxjs/operators';
 
 export enum ContentMaintenanceTabs {
     GENERAL = 'general',
@@ -14,7 +15,7 @@ export enum ContentMaintenanceTabs {
     styleUrls: ['./content-maintenance.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContentMaintenanceComponent {
+export class ContentMaintenanceComponent implements OnInit, OnDestroy {
 
     readonly ContentMaintenanceTabs = ContentMaintenanceTabs;
 
@@ -27,12 +28,41 @@ export class ContentMaintenanceComponent {
     selectedNodeIds: number[] = [];
 
     /** If TRUE components in this view poll and refresh the data they display in an intervall defined in `lifeSyncIntervall` */
-    lifeSyncEnabled = true;
+    lifeSyncEnabled = false;
 
     /** Determines the amount of seconds of components in this view between polling information. */
     lifeSyncIntervall = PUBLISH_PROCESS_REFRESH_INTERVAL;
 
+    private subscriptions: Subscription[] = [];
+
+    constructor(
+        private changeDetector: ChangeDetectorRef,
+        private appState: AppStateService,
+    ) {}
+
+    ngOnInit(): void {
+        this.subscriptions.push(combineLatest([
+            this.appState.select(state => state.auth.isLoggedIn),
+            this.appState.select(state => state.auth.currentUserId),
+        ]).pipe(
+            filter(([loggedIn]) => loggedIn),
+            mergeMap(([, userId]) => this.appState.select(state => state.ui.settings[userId])),
+        ).subscribe(userSettings => {
+            this.lifeSyncEnabled = userSettings.pollContentMaintenance ?? false;
+            this.changeDetector.markForCheck();
+        }));
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach(s => s.unsubscribe());
+    }
+
+    changeLifeSyncPoll(doPoll: boolean): void {
+        this.appState.dispatch(new SetUserSettingAction('pollContentMaintenance', doPoll));
+    }
+
     changeTab(activeTabId: ContentMaintenanceTabs): void {
         this.activeTabId = activeTabId;
+        this.changeDetector.markForCheck();
     }
 }

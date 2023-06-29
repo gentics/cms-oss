d2f4d8e18dbd93c02bb93e6dcda6d1cf7e80b6ef
Decker Dominik <d.decker@gentics.com>
2023-06-29 15:41:20 +0200

    SUP-15423: Added back option to restrict groups to nodes

--- cms-ui/apps/admin-ui/src/app/shared/components/assign-user-to-groups-modal/assign-user-to-groups-modal.component.ts
+++ cms-ui/apps/admin-ui/src/app/shared/components/assign-user-to-groups-modal/assign-user-to-groups-modal.component.ts
@@ -1,29 +1,71 @@
-import { I18nService } from '@admin-ui/core';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Normalized, Raw, User } from '@gentics/cms-models';
-import { BaseModal } from '@gentics/ui-core';
-import { Observable } from 'rxjs';
+import { GroupBO } from '@admin-ui/common';
+import { ErrorHandler, I18nService } from '@admin-ui/core';
+import { AppStateService } from '@admin-ui/state';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Feature, Normalized, Raw, User } from '@gentics/cms-models';
+import { BaseModal, ModalService, TableAction, TableActionClickEvent } from '@gentics/ui-core';
+import { Observable, Subscription } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 import { UserDataService } from '../../providers/user-data/user-data.service';
+import { AssignNodeRestrictionsToUsersModalComponent } from '../assign-node-restriction-to-users-modal/assign-node-restriction-to-users-modal.component';
+
+const ACTION_NODE_RESTRICTIONS = 'restrict-by-nodes';
 
 @Component({
     selector: 'gtx-assign-user-to-groups-modal',
     templateUrl: './assign-user-to-groups-modal.component.html',
+    styleUrls: ['./assign-user-to-groups-modal.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AssignUserToGroupsModalComponent extends BaseModal<User<Raw>[] | boolean> {
+export class AssignUserToGroupsModalComponent extends BaseModal<User<Raw>[] | boolean> implements OnInit, OnDestroy {
 
     /** IDs of users to be (un)assigned to/from groups */
-    userIds: number[] = [];
+    public userIds: number[] = [];
 
     /** IDs of groups to which the users shall finally be assigned to */
-    userGroupIds: number[] = [];
+    public userGroupIds: number[] = [];
+
+    public actions: TableAction<GroupBO>[] = [];
+
+    private subscriptions: Subscription[] = [];
 
     constructor(
+        private changeDetector: ChangeDetectorRef,
+        private appState: AppStateService,
         private userData: UserDataService,
         private i18n: I18nService,
+        private modalService: ModalService,
+        private errorHandler: ErrorHandler,
     ) {
         super();
+    }
+
+    ngOnInit(): void {
+        this.subscriptions.push(this.appState.select(state => state.features.global[Feature.MULTICHANNELLING]).subscribe(multiChanneling => {
+            if (!multiChanneling && this.userIds.length === 1) {
+                this.actions = [];
+                this.changeDetector.markForCheck();
+                return;
+            }
+
+            this.actions = [
+                {
+                    id: ACTION_NODE_RESTRICTIONS,
+                    label: this.i18n.instant('common.assign_user_node_restrictions'),
+                    enabled: true,
+                    icon: 'lock',
+                    type: 'secondary',
+                    single: true,
+                },
+            ];
+            this.changeDetector.markForCheck();
+        }));
+
+        this.subscriptions.push()
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach(s => s.unsubscribe());
     }
 
     /** Get form validity state */
@@ -48,6 +90,28 @@ export class AssignUserToGroupsModalComponent extends BaseModal<User<Raw>[] | bo
             return this.i18n.get('shared.assign_users_to_groups');
         }
     }
+
+    handleActionClick(event: TableActionClickEvent<GroupBO>): void {
+        switch (event.actionId) {
+            case ACTION_NODE_RESTRICTIONS:
+                this.getUserNodeRestrictions(event.item.id);
+                break;
+        }
+    }
+
+    async getUserNodeRestrictions(groupId: number): Promise<void> {
+        try {
+            const dialog = await this.modalService.fromComponent(
+                AssignNodeRestrictionsToUsersModalComponent,
+                { closeOnOverlayClick: false, width: '50%' },
+                { userId: this.userIds[0], groupId },
+            );
+            await dialog.open();
+        } catch (err) {
+            this.errorHandler.catch(err);
+        }
+    }
+
 
     /**
      * @param replace if the selected groupIds should replace the existing groups

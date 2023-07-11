05a7e182a95bded9ca308a81bbb67e84166d07cf
Oliver Guggenberger <O.Guggenberger@gentics.com>
2023-07-11 15:18:47 +0200

    Improvements, include feedback

--- cms-restapi/src/main/java/com/gentics/contentnode/rest/resource/PartTypeResource.java
+++ cms-restapi/src/main/java/com/gentics/contentnode/rest/resource/PartTypeResource.java
@@ -4,16 +4,26 @@ import com.gentics.contentnode.rest.model.PartType;
 import com.gentics.contentnode.rest.resource.parameter.FilterParameterBean;
 import java.util.List;
 import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+
+/**
+ * Resource for part types
+ */
+@Path("/parttype")
 public abstract class PartTypeResource {
 
-	/**
-	 * List Part types
-	 * @param filter filter parameters
-	 * @return list of part type
-	 * @throws Exception in case of errors
-	 */
-	public abstract List<PartType> list(
-			@BeanParam FilterParameterBean filter) throws Exception;
+  /**
+   * List Part types
+   *
+   * @param filter filter parameters
+   * @return list of part type
+   * @throws Exception in case of errors
+   */
+  @GET
+  public abstract List<PartType> list(
+      @BeanParam FilterParameterBean filter) throws Exception;
 
 }
+

ff4e4ac0bfd65259e9e7e1275577a07e29a06281
Oliver Guggenberger <O.Guggenberger@gentics.com>
2023-12-13 11:11:30 +0100

    Translate publish state

--- cms-ui/apps/admin-ui/src/app/features/mesh-browser/components/mesh-browser-content-version/mesh-browser-content-version.component.ts
+++ cms-ui/apps/admin-ui/src/app/features/mesh-browser/components/mesh-browser-content-version/mesh-browser-content-version.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, Input, OnInit } from '@angular/core';
+import { I18nService } from '@admin-ui/core';
 import { PublishedState, SchemaElement } from '../../models/mesh-browser-models';
 
 @Component({
@@ -16,19 +17,27 @@ export class MeshBrowserContentVersionComponent implements OnInit {
 
     public publishedState: PublishedState;
 
+    public readonly PublishedStateTranslations = new Map<string, string>([
+        [PublishedState.PUBLISHED, 'mesh.published_state_published'],
+        [PublishedState.ARCHIVED, 'mesh.published_state_archived'],
+        [PublishedState.DRAFT, 'mesh.published_state_draft'],
+        [PublishedState.UPDATED, 'mesh.published_state_updated'],
+    ]);
+
     public title = '';
 
-    constructor() { }
+    constructor(protected i18n: I18nService) { }
+
 
     ngOnInit(): void {
         this.publishedState = this.getPublishedState();
 
-        this.title = `version: ${this.schemaElement?.version}`;
+        this.title = `Version: ${this.schemaElement?.version}\n`;
 
         if (this.schemaElement?.versions?.length > 0) {
             const created = this.schemaElement.versions[0].created;
             const createdDate = new Date(created).toLocaleString()
-            this.title += ` last modified at: ${createdDate}`;
+            this.title += `${this.i18n.instant('mesh.published_state_changed')}: ${createdDate}`;
         }
     }
 

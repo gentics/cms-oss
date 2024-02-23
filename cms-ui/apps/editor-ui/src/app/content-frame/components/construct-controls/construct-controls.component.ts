248a1681276a294415caa49c1855ad5d0ff383fd
Decker Dominik <d.decker@gentics.com>
2024-02-23 12:59:58 +0100

    GPU-1286: Fixed construct insertion dropdown

--- cms-ui/apps/editor-ui/src/app/content-frame/components/construct-controls/construct-controls.component.ts
+++ cms-ui/apps/editor-ui/src/app/content-frame/components/construct-controls/construct-controls.component.ts
@@ -63,6 +63,7 @@ export class ConstructControlsComponent implements OnInit, OnChanges {
 
     protected gcnTags: GCNTags;
 
+    protected dropdownIsFavourites = false;
     protected currentlyOpenDropdown: DropdownListComponent | null;
 
     constructor(
@@ -114,11 +115,12 @@ export class ConstructControlsComponent implements OnInit, OnChanges {
         }
     }
 
-    public handleDropdownOpen(instance: DropdownListComponent): void {
+    public handleDropdownOpen(instance: DropdownListComponent, isFavourites: boolean): void {
         if (this.currentlyOpenDropdown?.isOpen) {
             this.currentlyOpenDropdown.closeDropdown();
         }
         this.currentlyOpenDropdown = instance;
+        this.dropdownIsFavourites = isFavourites;
     }
 
     public insertConstruct(construct: Construct, event?: Event): void {
@@ -198,6 +200,14 @@ export class ConstructControlsComponent implements OnInit, OnChanges {
     protected updateFavouriteConstructs(): void {
         this.favouriteConstructs = this.availableConstructs
             .filter(construct => (this.favourites || []).includes(construct.keyword));
+
+        if (this.currentlyOpenDropdown != null && this.dropdownIsFavourites) {
+            if (this.favourites.length === 0) {
+                this.currentlyOpenDropdown.closeDropdown();
+            } else {
+                this.currentlyOpenDropdown.resize();
+            }
+        }
     }
 
     protected updateDisplayGroups(): void {

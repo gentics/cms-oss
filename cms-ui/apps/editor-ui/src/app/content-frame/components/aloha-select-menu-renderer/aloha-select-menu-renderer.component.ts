727be1e81f90f923989fbf67e91972be528d3415
Decker Dominik <d.decker@gentics.com>
2024-02-29 18:00:59 +0100

    GPU-1418: Select Menu multi step

--- cms-ui/apps/editor-ui/src/app/content-frame/components/aloha-select-menu-renderer/aloha-select-menu-renderer.component.ts
+++ cms-ui/apps/editor-ui/src/app/content-frame/components/aloha-select-menu-renderer/aloha-select-menu-renderer.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { AlohaSelectMenuComponent, SelectMenuOption, SelectMenuSelectEvent } from '@gentics/aloha-models';
+import { AlohaSelectMenuComponent, MultiStepSelectMenuOption, SelectMenuOption, SelectMenuSelectEvent } from '@gentics/aloha-models';
 import { generateFormProvider } from '@gentics/ui-core';
+import { Subscription, combineLatest } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { BaseAlohaRendererComponent } from '../base-aloha-renderer/base-aloha-renderer.component';
+import { applyControl } from '../../utils';
 
 @Component({
     selector: 'gtx-aloha-select-menu-renderer',
@@ -11,11 +14,36 @@ import { BaseAlohaRendererComponent } from '../base-aloha-renderer/base-aloha-re
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [generateFormProvider(AlohaSelectMenuRendererComponent)],
 })
-export class AlohaSelectMenuRendererComponent extends BaseAlohaRendererComponent<AlohaSelectMenuComponent, SelectMenuSelectEvent<any>> {
+export class AlohaSelectMenuRendererComponent
+    extends BaseAlohaRendererComponent<AlohaSelectMenuComponent, SelectMenuSelectEvent<any>>
+    implements OnInit, OnDestroy {
 
-    public activeMultiStep: SelectMenuOption | null = null;
+    @Output()
+    public multiStepActivation = new EventEmitter<string>();
+
+    @ViewChild('nextStep')
+    public nextStep: ElementRef<HTMLDivElement>;
+
+    public activeMultiStep: MultiStepSelectMenuOption<any> | null = null;
     public componentRequiresConfirm = false;
     public control: FormControl<any>;
+    public hasLeftIcon = false;
+    public hasRightIcon = false;
+    public nextStepReady = false;
+
+    protected ctlSub: Subscription[] = [];
+
+    public ngOnInit(): void {
+        super.ngOnInit();
+
+        this.checkIcons();
+    }
+
+    public ngOnDestroy(): void {
+        super.ngOnDestroy();
+
+        this.clearControlSubscriptions();
+    }
 
     protected override setupAlohaHooks(): void {
         super.setupAlohaHooks();
@@ -30,33 +58,95 @@ export class AlohaSelectMenuRendererComponent extends BaseAlohaRendererComponent
         };
         this.settings.setOptions = (options) => {
             this.settings.options = options;
+            this.checkIcons();
             this.changeDetector.markForCheck();
         };
     }
 
+    protected checkIcons(): void {
+        const arr = (this.settings?.options || []) as SelectMenuOption[];
+        this.hasLeftIcon = false;
+        this.hasRightIcon = false;
+
+        for (const opt of arr) {
+            if (opt.icon) {
+                this.hasLeftIcon = true;
+            }
+            if (opt.isMultiStep) {
+                this.hasRightIcon = true;
+            }
+            if (this.hasLeftIcon && this.hasRightIcon) {
+                break;
+            }
+        }
+    }
+
     public handleOptionClick(option: SelectMenuOption): void {
         if (!option.isMultiStep) {
-            this.activeMultiStep = null;
-            this.control = null;
+            this.nextStepReady = false;
+            this.element.nativeElement.style.height = '';
+            this.element.nativeElement.style.width = '';
+
+            if (this.activeMultiStep) {
+                this.clearControlSubscriptions();
+                this.activeMultiStep = null;
+                this.control = null;
+            }
+
             // eslint-disable-next-line @typescript-eslint/no-unsafe-call
             this.settings.onSelect?.({ id: option.id });
             this.triggerChange({ id: option.id });
             return;
         }
 
-        this.componentRequiresConfirm = false;
+        this.clearControlSubscriptions();
+        this.componentRequiresConfirm = option.multiStepContext.requiresConfirm;
         this.control = new FormControl(option.multiStepContext.initialValue);
+        const sub = applyControl(this.control, option.multiStepContext);
+        if (sub) {
+            this.ctlSub.push(sub);
+        }
+
+        this.ctlSub.push(combineLatest([
+            this.control.valueChanges,
+            this.control.statusChanges,
+        ]).pipe(
+            filter(([_, status]) => status === 'VALID'),
+            map(([value]) => value),
+        ).subscribe(() => {
+            if (this.componentRequiresConfirm) {
+                return;
+            }
+            this.triggerMultiStepValueChange();
+        }));
         this.activeMultiStep = option;
+
+        setTimeout(() => {
+            // const rect = this.nextStep.nativeElement.getBoundingClientRect();
+            // this.element.nativeElement.style.height = `${rect.height}px`;
+            // this.element.nativeElement.style.width = `${rect.width}px`;
+
+            this.nextStepReady = true;
+            this.changeDetector.markForCheck();
+        });
+
+        this.multiStepActivation.emit(this.activeMultiStep.id);
     }
 
     public stepBack(): void {
         this.activeMultiStep = null;
+        this.multiStepActivation.emit(null);
+    }
+
+    protected triggerMultiStepValueChange(): void {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        this.settings?.onSelect?.({ id: this.activeMultiStep.id, value: this.control.value });
+        this.triggerChange({ id: this.activeMultiStep.id, value: this.control.value });
     }
 
     public multiStepConfirm(): void {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        this.settings?.onSelect({ id: this.activeMultiStep.id, value: this.control.value });
-        this.triggerChange({ id: this.activeMultiStep.id, value: this.control.value });
+        this.triggerMultiStepValueChange();
+        this.manualConfirm.emit();
     }
 
     public handleComponentRequire(confirm: boolean): void {
@@ -65,5 +155,10 @@ export class AlohaSelectMenuRendererComponent extends BaseAlohaRendererComponent
 
     public handleComponentManualConfirm(): void {
         this.multiStepConfirm();
+    }
+
+    protected clearControlSubscriptions(): void {
+        this.ctlSub.forEach(s => s.unsubscribe());
+        this.ctlSub = [];
     }
 }

8cc7819a0aae6158722bb14955af2b6d34afcf0c
Oliver Guggenberger <O.Guggenberger@gentics.com>
2024-04-25 15:29:52 +0200

    Add button and logic to unassign languages from nodes

--- cms-ui/apps/admin-ui/src/app/shared/components/language-table/language-table.component.ts
+++ cms-ui/apps/admin-ui/src/app/shared/components/language-table/language-table.component.ts
@@ -3,12 +3,16 @@ import { I18nService, LanguageLoaderOptions, LanguageTableLoaderService, Permiss
 import { AppStateService } from '@admin-ui/state';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { AnyModelType, Language, NormalizableEntityTypesMap } from '@gentics/cms-models';
-import { ModalService, TableAction, TableColumn } from '@gentics/ui-core';
+import { ModalService, TableAction, TableActionClickEvent, TableColumn } from '@gentics/ui-core';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DELETE_ACTION } from '../base-entity-table/base-entity-table.component';
 import { BaseSortableEntityTableComponent } from '../base-sortable-entity-table/base-sortable-entity-table.component';
 import { CreateLanguageModalComponent } from '../create-language-modal/create-language-modal.component';
+
+
+export const UNASSIGN_ACTION = 'unassign';
+
 
 @Component({
     selector: 'gtx-language-table',
@@ -41,6 +45,7 @@ export class LanguageTableComponent
         },
     ];
     protected entityIdentifier: keyof NormalizableEntityTypesMap<AnyModelType> = 'language';
+    protected languageLoader: LanguageTableLoaderService;
 
     constructor(
         changeDetector: ChangeDetectorRef,
@@ -57,6 +62,7 @@ export class LanguageTableComponent
             loader,
             modalService,
         );
+        this.languageLoader = loader;
     }
 
     public override ngOnChanges(changes: SimpleChanges): void {
@@ -78,6 +84,15 @@ export class LanguageTableComponent
 
                 if (this.sorting) {
                     actions.push(...createMoveActions(this.i18n, true));
+                    actions.push({
+                        id: UNASSIGN_ACTION,
+                        icon: 'link_off',
+                        label: this.i18n.instant('shared.unassign_languages_from_node'),
+                        type: 'alert',
+                        enabled: true,
+                        multiple: true,
+                        single: true,
+                    });
                 } else {
                     actions.push({
                         id: DELETE_ACTION,
@@ -117,5 +132,17 @@ export class LanguageTableComponent
 
     handleNodeLanguageAssignment(): void {
         this.assignToNode.emit();
+    }
+
+    override handleAction(event: TableActionClickEvent<LanguageBO>): void {
+        super.handleAction(event);
+
+        switch (event.actionId) {
+            case UNASSIGN_ACTION:
+                this.languageLoader.unassignLanguageFromNode(this.nodeId, event.item.id);
+                this.languageLoader.reload();
+                break;
+        }
+
     }
 }

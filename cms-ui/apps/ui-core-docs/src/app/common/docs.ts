24a22f688d33c5ec4000d81de566ea19a3e8b856
Decker Dominik <d.decker@gentics.com>
2024-05-06 16:25:43 +0200

    Added generics info; highlightjs fixes; Cleanup

--- cms-ui/apps/ui-core-docs/src/app/common/docs.ts
+++ cms-ui/apps/ui-core-docs/src/app/common/docs.ts
@@ -25,13 +25,18 @@ export interface InheritanceInfo {
 
 export interface SourceFile {
     type: 'component' | 'service';
+    /** File path to the source-file to load the contents from. */
     sourceFile: string;
+    /** Which class it extends. Used for referencing and linking. */
     extends?: string;
+    /** The path where this documentation page is available at. Used in base-classes for linking. */
+    path?: string;
 }
 
 export interface IDocumentation extends SourceFile {
     name: string;
     main: string;
+    generics?: string[];
     inheritance?: InheritanceInfo[];
     inputs: DocBlock[];
     outputs: DocBlock[];

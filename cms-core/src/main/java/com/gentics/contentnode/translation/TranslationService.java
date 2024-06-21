4ebc5e94bb8b69a6aacf96f16df1c5f1d8fa374a
Oliver Guggenberger <O.Guggenberger@gentics.com>
2024-06-21 12:53:23 +0200

    GPU-1710: Add missing javadoc & improvements

--- cms-core/src/main/java/com/gentics/contentnode/translation/TranslationService.java
+++ cms-core/src/main/java/com/gentics/contentnode/translation/TranslationService.java
@@ -1,36 +1,52 @@
 package com.gentics.contentnode.translation;
 
 import com.gentics.api.lib.exception.NodeException;
-import com.gentics.contentnode.etc.Function;
-import com.gentics.contentnode.object.StageableNodeObject;
-import com.gentics.contentnode.rest.model.response.StagingStatus;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.function.Predicate;
 
 /**
- * Interface for the translation service.
+ * Interface for translation services
  */
 public interface TranslationService {
 	/**
 	 * Translate a text to the specified language by specifying the source language.
-	 * 
-	 * @param textToTranslate The text that should be translated
-	 * @param sourceLanguage The language of the source text.
-	 * @param targetLanguage The language that the text should be translated to.
-	 * @return The translated text
-	 * @throws NodeException
+	 *
+	 * @param textToTranslate the text that should be translated
+	 * @param sourceLanguage  the language of the source text.
+	 * @param targetLanguage  the language that the text should be translated to.
+	 * @return the translated text
+	 * @throws NodeException if an error occurs during translating
 	 */
-	String translate(String textToTranslate, String sourceLanguage, String targetLanguage) throws NodeException;
+	String translate(String textToTranslate, String sourceLanguage, String targetLanguage)
+			throws NodeException;
+
+
+	/**
+	 * @param htmlToTranslate the text containing markup to translate
+	 * @param sourceLanguage  the language of the source text.
+	 * @param targetLanguage  the language that the text should be translated to.
+	 * @param validator       a validator function that checks if the markup is valid
+	 * @return The translated text
+	 * @throws NodeException if the translated markup fails the validation or an error occur during translating.
+	 */
+	default String translate(String htmlToTranslate, String sourceLanguage, String targetLanguage,
+			Predicate<String> validator) throws NodeException {
+		var translatedMarkup = this.translate(htmlToTranslate, sourceLanguage, targetLanguage);
+
+		if (!validator.test(translatedMarkup)) {
+			throw new NodeException("Translated markup is invalid");
+		}
+
+		return translatedMarkup;
+	}
 
 
 	/**
 	 * Translate a text to the specified language. The source language will be autodetected.
 	 *
-	 * @param textToTranslate The text that should be translated
-	 * @param targetLanguage The language that the text should be translated to.
-	 * @return The translated text
-	 * @throws NodeException
+	 * @param textToTranslate the text that should be translated
+	 * @param targetLanguage  the language that the text should be translated to.
+	 * @return the translated text
+	 * @throws NodeException if an error occurs during translating
 	 */
 	String translate(String textToTranslate, String targetLanguage) throws NodeException;
 }

a677840fca105afa2e2431c118aa385154966dbd
Oliver Guggenberger <O.Guggenberger@gentics.com>
2024-08-22 13:56:45 +0200

    GPU-1794: Add generic client error translation

--- base-lib/src/main/java/com/gentics/api/lib/exception/TranslationException.java
+++ base-lib/src/main/java/com/gentics/api/lib/exception/TranslationException.java
@@ -1,7 +1,10 @@
 package com.gentics.api.lib.exception;
 
+import java.io.Serial;
+
 public class TranslationException extends NodeException {
 
+	@Serial
 	private static final long serialVersionUID = -4897057478604156346L;
 
 	public TranslationException() {

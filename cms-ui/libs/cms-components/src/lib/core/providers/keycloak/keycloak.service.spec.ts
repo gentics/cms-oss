62323b3c44c2de0c70759f1c848434fad4994592
Decker Dominik <d.decker@gentics.com>
2024-09-04 12:01:30 +0200

    Fixed UI tests/building

--- cms-ui/libs/cms-components/src/lib/core/providers/keycloak/keycloak.service.spec.ts
+++ cms-ui/libs/cms-components/src/lib/core/providers/keycloak/keycloak.service.spec.ts
@@ -1,29 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { Store } from '@ngxs/store';
-import { AppStateService } from '../../../../../../../apps/admin-ui/src/app/state';
-import { TestAppState } from '../../../../../../../apps/admin-ui/src/app/state/utils/test-app-state';
-import { MockStore } from '../../../../../../../apps/admin-ui/src/app/state/utils/test-app-state/test-store.mock';
+import { RouterModule } from '@angular/router';
+import { GCMSRestClientService } from '@gentics/cms-rest-client-angular';
+import { GCMSTestRestClientService } from '@gentics/cms-rest-client-angular/testing';
 import { KeycloakService } from './keycloak.service';
 
-class MockHttpClient {}
-
 describe('KeycloakService', () => {
-
-    let appState: TestAppState;
     let keycloakService: KeycloakService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 KeycloakService,
-                { provide: AppStateService, useClass: TestAppState },
-                { provide: HttpClient, useClass: MockHttpClient },
-                { provide: Store, useClass: MockStore },
+                {
+                    provide: GCMSRestClientService,
+                    useClass: GCMSTestRestClientService,
+                },
             ],
+            imports: [RouterModule.forRoot([])],
         });
 
-        appState = TestBed.get(AppStateService);
         keycloakService = TestBed.get(KeycloakService);
     });
 

cbfdff7ad3b944daf9868b3a95a6b70cda53976c
Decker Dominik <d.decker@gentics.com>
2025-03-20 16:18:24 +0100

    GPU-1953: Using official image for playwright server

--- cms-integration-tests/playwright/healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl \
-    --include \
-    --no-buffer \
-    --header 'Connection: close' \
-    --header 'Upgrade: websocket' \
-    --header 'Sec-WebSocket-Version: 13' \
-    http://localhost:3000

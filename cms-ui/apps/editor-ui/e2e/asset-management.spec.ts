053544f0b9af51a27f8f1413b24c6efc52779f16
Decker Dominik <d.decker@gentics.com>
2025-07-30 17:47:03 +0200

    SUP-18778: Fixed integration tests

--- cms-ui/apps/editor-ui/e2e/asset-management.spec.ts
+++ cms-ui/apps/editor-ui/e2e/asset-management.spec.ts
@@ -16,69 +16,95 @@ test.describe('Asset Management', () => {
 
         await IMPORTER.bootstrapSuite(TestSize.MINIMAL);
         await IMPORTER.bootstrapSuite(TestSize.FULL);
+    });
 
+    test.beforeEach(async ({ request, context }) => {
+        IMPORTER.setApiContext(request);
+        await context.clearCookies();
+        await IMPORTER.clearClient();
+
+        await IMPORTER.cleanupTest();
+
+        await IMPORTER.setupTest(TestSize.MINIMAL);
         await IMPORTER.setupFeatures(TestSize.MINIMAL, {
             [NodeFeature.ASSET_MANAGEMENT]: true,
         });
-        await IMPORTER.setupFeatures(TestSize.FULL, {
-            [NodeFeature.ASSET_MANAGEMENT]: true,
+    });
+
+    test.describe('Multiple Nodes required', () => {
+        test.beforeEach(async ({ page }) => {
+            await IMPORTER.setupTest(TestSize.FULL);
+            await IMPORTER.setupFeatures(TestSize.FULL, {
+                [NodeFeature.ASSET_MANAGEMENT]: true,
+            });
+
+            await navigateToApp(page);
+            await loginWithForm(page, AUTH.admin);
+        });
+
+        test('should have asset-management available in both nodes', {
+            annotation: [{
+                type: 'ticket',
+                description: 'SUP-18778',
+            }],
+        }, async ({ page }) => {
+            await test.step('open asset-management in minimal node', async () => {
+                await selectNode(page, IMPORTER.get(minimalNode)!.id);
+                const list = findList(page, 'image');
+                const picker = list.locator('[data-action="upload-item"] gtx-dropdown-list');
+                const context = await openContext(picker);
+                expect(context.locator('gtx-dropdown-item')).toHaveCount(2);
+
+                // Close the dropdown
+                await page.click('gtx-scroll-mask');
+            });
+
+            await test.step('open asset-management in full node', async () => {
+                await selectNode(page, IMPORTER.get(fullNode)!.id);
+                const list = findList(page, 'image');
+                const picker = list.locator('[data-action="upload-item"] gtx-dropdown-list');
+                const context = await openContext(picker);
+                expect(context.locator('gtx-dropdown-item')).toHaveCount(2);
+
+                // Close the dropdown
+                await page.click('gtx-scroll-mask');
+            });
         });
     });
 
-    test.beforeEach(async ({ page, request, context }) => {
-        await context.clearCookies();
-        IMPORTER.setApiContext(request);
-        await IMPORTER.clearClient();
-        await IMPORTER.cleanupTest();
-        await IMPORTER.setupTest(TestSize.MINIMAL);
-        await navigateToApp(page);
-        await loginWithForm(page, AUTH.admin);
-    });
+    test.describe('Single Node', () => {
+        test.beforeEach(async ({ page }) => {
+            await navigateToApp(page);
+            await loginWithForm(page, AUTH.admin);
+        });
 
-    test('should have asset-management available in both nodes', {
-        annotation: [{
-            type: 'ticket',
-            description: 'SUP-18778',
-        }],
-    }, async ({ page }) => {
-        await test.step('open asset-management in minimal node', async () => {
+        test('should be able to use the custom asset manager', {
+            annotation: [{
+                type: 'ticket',
+                description: 'SUP-18778',
+            }],
+        }, async ({ page }) => {
             await selectNode(page, IMPORTER.get(minimalNode)!.id);
             const list = findList(page, 'image');
-            const picker = list.locator('[data-action-id="upload-item"] gtx-dropdown-list');
+            const picker = list.locator('[data-action="upload-item"] gtx-dropdown-list');
             const context = await openContext(picker);
-            expect(context.locator('gtx-dropdown-item')).toHaveCount(2);
+
+            const saveReq = page.waitForResponse(data =>
+                data.request().method() === 'POST' && matchesPath(data.request().url(), '/rest/file/create'),
+            );
+            const loadReq = page.waitForResponse(data =>
+                data.request().method() === 'GET' && matchesPath(data.request().url(), '/rest/image/load/*'),
+            );
+            await context.locator('[data-provider-id="custom"] gtx-button button').click();
+            const assetManager = page.frameLocator('external-assets-modal iframe');
+            await assetManager.locator('button').click();
+
+            const res = await saveReq;
+            const body: FileUploadResponse = await res.json();
+
+            await loadReq;
+
+            await findItem(list, body.file.id).waitFor();
         });
-
-        await test.step('open asset-management in full node', async () => {
-            await selectNode(page, IMPORTER.get(fullNode)!.id);
-            const list = findList(page, 'image');
-            const picker = list.locator('[data-action-id="upload-item"] gtx-dropdown-list');
-            const context = await openContext(picker);
-            expect(context.locator('gtx-dropdown-item')).toHaveCount(2);
-        });
-    });
-
-    test('should be able to use the custom asset manager', {
-        annotation: [{
-            type: 'ticket',
-            description: 'SUP-18778',
-        }],
-    }, async ({ page }) => {
-        await selectNode(page, IMPORTER.get(minimalNode)!.id);
-        const list = findList(page, 'image');
-        const picker = list.locator('[data-action-id="upload-item"] gtx-dropdown-list');
-        const context = await openContext(picker);
-
-        const saveReq = page.waitForResponse(data =>
-            matchesPath(data.request().url(), '/rest/file/create') && data.request().method() === 'POST',
-        );
-        await context.locator('[data-provider-id="custom"] gtx-button button').click();
-        const assetManager = page.frameLocator('external-assets-modal iframe');
-        await assetManager.locator('button').click();
-
-        const res = await saveReq;
-        const body: FileUploadResponse = await res.json();
-
-        await expect(findItem(list, body.file.id)).toBeAttached();
     });
 });

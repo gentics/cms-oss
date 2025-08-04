602e90d41ed13e2bcf036913dc5a59684aa10208
Serhii Plyhun <S.Plyhun@gentics.com>
2025-08-04 14:47:37 +0200

    SUP-18810: Reset selected items before/after the Content Staging mode

--- cms-ui/apps/editor-ui/src/app/core/components/content-staging-modal/content-staging-modal.component.ts
+++ cms-ui/apps/editor-ui/src/app/core/components/content-staging-modal/content-staging-modal.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { UIMode } from '@editor-ui/app/common/models';
-import { ApplicationStateService, SetActiveContentPackageAction, SetUIModeAction } from '@editor-ui/app/state';
+import { ApplicationStateService, SetActiveContentPackageAction, SetUIModeAction, ChangeListSelectionAction } from '@editor-ui/app/state';
 import { BaseModal } from '@gentics/ui-core';
 
 @Component({
@@ -8,7 +8,7 @@ import { BaseModal } from '@gentics/ui-core';
     templateUrl: './content-staging-modal.component.html',
     styleUrls: ['./content-staging-modal.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    })
+})
 export class ContentStagingModal extends BaseModal<void> implements OnInit {
 
     public hadPackage = false;
@@ -33,6 +33,7 @@ export class ContentStagingModal extends BaseModal<void> implements OnInit {
 
     updatePackageInState(): void {
         if (this.activePackage) {
+            this.state.dispatch(new ChangeListSelectionAction(null, 'clear'));
             this.state.dispatch(new SetUIModeAction(UIMode.STAGING));
             this.state.dispatch(new SetActiveContentPackageAction(this.activePackage));
         } else {

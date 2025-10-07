8124ae3f24922ea0bce110c6792607b50941eac7
Dominik Decker <d.decker@gentics.com>
2025-10-07 17:26:48 +0200

    SUP-19023: Testing cleanup

--- cms-ui/apps/admin-ui/e2e/dev-tool-packages.spec.ts
+++ cms-ui/apps/admin-ui/e2e/dev-tool-packages.spec.ts
@@ -1,25 +1,20 @@
-import {ObjectProperty, Template} from '@gentics/cms-models';
+import { ObjectProperty } from '@gentics/cms-models';
 import {
-    BASIC_TEMPLATE_ID,
     EntityImporter,
+    OBJECT_PROPERTY_PAGE_COLOR,
     TestSize,
+    clickTableRow,
     findTableRowById,
     loginWithForm,
-    navigateToApp, OBJECT_PROPERTY_PAGE_COLOR, matchRequest, clickTableRow, selectTableRow,
+    navigateToApp,
+    selectTableRow,
 } from '@gentics/e2e-utils';
 import { expect, test } from '@playwright/test';
 import { AUTH } from './common';
-import {navigateToModule, selectTab} from './helpers';
-
-const NODE_NAME = 'empty node';
-const TEMPLATE_NAME = '[Test] Basic Template';
-const LINK_TO_NODE_ACTION = 'linkToNode';
-const LINK_TO_NODE_MODAL = 'gtx-assign-templates-to-nodes-modal';
-const LINK_TO_FOLDER_ACTION = 'linkToFolder';
-const LINK_TO_FOLDER_MODAL = 'gtx-assign-templates-to-folders-modal';
+import { navigateToModule, selectTab } from './helpers';
 
 test.describe.configure({ mode: 'serial' });
-test.describe('Packages Module', () => {
+test.describe('Dev-Tool Packages Module', () => {
     const IMPORTER = new EntityImporter();
 
     let objectProperty: ObjectProperty;
@@ -48,7 +43,7 @@ test.describe('Packages Module', () => {
         await navigateToModule(page, 'devtool-packages');
     });
 
-    test('should show package', async ({ page }) => {
+    test('should be possible to add a object-property to package', async ({ page }) => {
         const TEST_PACKAGE_NAME = 'Test Package';
         const master = page.locator('gtx-dev-tool-package-master');
         const masterTable = master.locator('gtx-dev-tool-package-table');
@@ -65,27 +60,22 @@ test.describe('Packages Module', () => {
         });
 
         await test.step('Assign object property to test package', async () => {
-            const row = findTableRowById(masterTable, TEST_PACKAGE_NAME);
-
-            await clickTableRow(row);
+            const packageRow = findTableRowById(masterTable, TEST_PACKAGE_NAME);
+            await clickTableRow(packageRow);
 
             const packageEditor = page.locator('gtx-dev-tool-package-editor');
-
             await selectTab(page, 'object-properties');
-
             const tabContents = packageEditor.locator('.is-active');
-            const selectObjectPropertyTable = tabContents.locator('gtx-object-property-table');
 
-            await selectObjectPropertyTable.locator('.entity-table-actions-bar [data-action="assign"] button').click();
+            const packagePropertiesTable = tabContents.locator('gtx-object-property-table');
+            await packagePropertiesTable.locator('.entity-table-actions-bar [data-action="assign"] button').click();
 
             const assignModal = page.locator('gtx-assign-entity-to-package-modal');
             const objectPropertyRow = findTableRowById(assignModal, objectProperty.id);
-
             await selectTableRow(objectPropertyRow);
-            await assignModal.locator('gtx-button[data-action="confirm"]').click();
+            await assignModal.locator('.modal-footer [data-action="confirm"] button').click();
 
-            const assignedObjectPropertyRow = findTableRowById(selectObjectPropertyTable, objectProperty.id);
-
+            const assignedObjectPropertyRow = findTableRowById(packagePropertiesTable, objectProperty.id);
             await expect(assignedObjectPropertyRow).toBeVisible();
         });
     });

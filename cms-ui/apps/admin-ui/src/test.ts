2f0f08a1c40cbba6d69dd8f258c9f23b6fbe7b97
Dominik Decker <d.decker@gentics.com>
2026-06-25 16:44:55 +0200

    GPU-2455: Fixed admin-ui unit-tests

--- cms-ui/apps/admin-ui/src/test.ts
+++ cms-ui/apps/admin-ui/src/test.ts
@@ -1,16 +1,20 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-// zone.js needs to be imported first, so we disable automatic import ordering.
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import {
     BrowserTestingModule,
     platformBrowserTesting,
 } from '@angular/platform-browser/testing';
+import 'zone.js';
 import 'zone.js/testing';
+
+@NgModule({
+    providers: [provideZoneChangeDetection()],
+})
+class AppTestingModule {}
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-    BrowserTestingModule,
+    [BrowserTestingModule, AppTestingModule],
     platformBrowserTesting(), {
         teardown: { destroyAfterEach: false },
     },

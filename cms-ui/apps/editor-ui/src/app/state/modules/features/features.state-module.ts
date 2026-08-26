dbd03d8221f1d67e4f7e2ab7549be60f03707359
Dominik Decker <d.decker@gentics.com>
2026-08-26 16:24:02 +0200

    Fixed building and unit-tests

--- cms-ui/apps/editor-ui/src/app/state/modules/features/features.state-module.ts
+++ cms-ui/apps/editor-ui/src/app/state/modules/features/features.state-module.ts
@@ -27,6 +27,7 @@ const INTIIAL_FEATURES_STATE: FeaturesState = {
     [Feature.FOLDER_BASED_TEMPLATE_SELECTION]: false,
     [Feature.CONTENT_STAGING]: false,
     [Feature.MULTICHANNELLING]: false,
+    [Feature.PARTIAL_MULTICHANNELLING]: false,
     [Feature.MESH_CR]: false,
     [Feature.OBJECT_TAG_SYNC]: false,
     [Feature.LINK_CHECKER]: false,

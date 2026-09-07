e4db32f46577770f8e5718e5188a84f2a25fec49
Norbert Pomaroli <n.pomaroli@gentics.com>
2026-09-07 13:47:01 +0200

    Fix adding the current tag to the resolving stack

--- cms-core/src/test/java/com/gentics/contentnode/tests/parttype/handlebars/HandlebarsPartTypeResolvingTest.java
+++ cms-core/src/test/java/com/gentics/contentnode/tests/parttype/handlebars/HandlebarsPartTypeResolvingTest.java
@@ -64,6 +64,8 @@ import com.gentics.contentnode.testutils.DBTestContext;
 public class HandlebarsPartTypeResolvingTest {
 	public final static String TAG_NAME = "testtag";
 
+	public final static String HBS_PART_NAME = "hb";
+
 	@ClassRule
 	public static DBTestContext testContext = new DBTestContext();
 
@@ -137,7 +139,7 @@ public class HandlebarsPartTypeResolvingTest {
 				p.setPartTypeId(getPartTypeId(HandlebarsPartType.class));
 				p.setEditable(0);
 				p.setHidden(false);
-				p.setKeyname("hb");
+				p.setKeyname(HBS_PART_NAME);
 				p.setName("Handlebars", 1);
 				p.setDefaultValue(create(Value.class, v -> {}).doNotSave().build());
 			}).doNotSave().build());
@@ -153,7 +155,7 @@ public class HandlebarsPartTypeResolvingTest {
 		}).build();
 
 		construct = update(construct, c -> {
-			getPartType(HandlebarsPartType.class, c, "hb").setText("{{#each cms.tag.parts.otherpart}}{{@key}}{{#unless @last}}, {{/unless}}{{/each}}");
+			getPartType(HandlebarsPartType.class, c, HBS_PART_NAME).setText("{{#each cms.tag.parts.otherpart}}{{@key}}{{#unless @last}}, {{/unless}}{{/each}}");
 		}).build();
 
 		template = create(Template.class, t -> {
@@ -199,6 +201,16 @@ public class HandlebarsPartTypeResolvingTest {
 	public void test() throws NodeException {
 		try (Trx trx = new Trx(); RenderTypeTrx rTrx = RenderTypeTrx.publish()) {
 			assertThat(page.render()).as("Rendered page").isEqualTo(expectedResolvableKeys);
+			trx.success();
+		}
+	}
+
+	@Test
+	public void testRenderPart() throws NodeException {
+		try (Trx trx = new Trx(); RenderTypeTrx rTrx = RenderTypeTrx.publish()) {
+			assertThat(page.render("<node %s.parts.%s>".formatted(TAG_NAME, HBS_PART_NAME),
+					trx.getTransaction().getRenderResult(), null, null, null, null)).as("Rendered page")
+					.isEqualTo(expectedResolvableKeys);
 			trx.success();
 		}
 	}
